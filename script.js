5d8bee741132172f6d2759808f0c6b64bcf8849f
Nez Mustafa <nezmustafa123@gmail.com>
2022-01-11 14:26:46 +0000

    navigator api

---
+++ script.js
@@ -0,0 +1,21 @@
+const btn = document.querySelector(".btn");
+const coupon = document.querySelector(".coupon");
+
+const copyText = (e) => {
+  //button is in a form so have to prevent default
+  e.preventDefault();
+  //select what's inside the input
+  //   console.log("hello");
+  //   coupon.select();
+  //   coupon.setSelectionRange(0, 99999999999);
+  //   coupon.cloneNode();
+  //select the coupon and define where you want to start and end selection
+
+  navigator.clipboard.writeText(coupon.value);
+  btn.textContent = "Copied!";
+  setTimeout(() => {
+    btn.textContent = "Copy";
+  }, 3000);
+};
+
+btn.addEventListener("click", copyText);
